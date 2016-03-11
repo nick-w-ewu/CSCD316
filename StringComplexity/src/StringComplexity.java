a0710d5dd72d7e1c6b3ae9a4023ad925bdfe598c
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2016-03-11 08:29:25 -0800

    Supervisors finished and string complexity

---
+++ StringComplexity/src/StringComplexity.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by nicho_000 on 3/5/2016.
+ */
+public class StringComplexity
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String test;
+        HashMap<String, Integer> letters;
+        while(input.hasNext())
+        {
+            test = input.nextLine();
+            letters = generateMap(test.toCharArray());
+            if(letters.size() > 2)
+            {
+                System.out.println(Math.abs(2 - letters.size()));
+            }
+            else
+            {
+                System.out.println(0);
+            }
+        }
+    }
+
+    private static HashMap<String, Integer> generateMap(char[] word)
+    {
+        HashMap<String, Integer> toReturn = new HashMap<>();
+        Integer count;
+        for(int i = 0; i < word.length; i++)
+        {
+            count = toReturn.get("" + word[i]);
+            if(count != null)
+            {
+                toReturn.put("" + word[i], count+1);
+            }
+            else
+            {
+                toReturn.put("" + word[i], 1);
+            }
+        }
+        return toReturn;
+    }
+}
